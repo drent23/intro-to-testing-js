dbc9f105f2f1a91c9ce95719549aab272369d2e1
David Renteria <david.j.renteria1@gmail.com>
2023-01-26 09:41:20 -0600

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -3,9 +3,17 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello() {
-    return "";
+// This means making sure that the sayHello function definition inside of code.js takes an an input argument. If input === "Alex", then we return "Hello, Alex!" else return "Hello, Jane!"
+function sayHello(input) {
+    if (input === "Alex") {
+        return "Hello, Alex!";
+    } else if (input === "Pat") {
+        return "Hello, Pat!";
+    } else {
+        return "Hello, Jane!";
+    }
 }
+
 
 
 
--- tests.js
+++ tests.js
@@ -16,7 +16,11 @@ describe('helloWorld', function() {
 
 // Open up tests.js. Add a describe, an it, and an expect to assert that sayHello is a defined function
 // Inside of tests.js, add an assertion to sayHello that it "should return a string when called.". The test should look similar to expect(typeof sayHello()).toBe("string")
-//
+//  In tests.js, assert that sayHello("Jane") returns "Hello, Jane!". Our first test should be super simple and super small.
+// Step 2: Run all tests and make sure that this newly added test is red.
+// Step 3: If the test wants us to return "Hello, Jane!" then literally write return "Hello, Jane!"; because that's the simplest way to green a test looking for "Hello, Jane!".
+// In tests.js, assert that sayHello("Alex") returns "Hello, Alex!". Our first test should be super simple and super small. This means that our next test should look like expect(sayHello("Alex")).toBe("Hello, Alex!").
+// Add another (tiny) assertion! In tests.js, assert that sayHello("Pat") returns "Hello, Pat!". Since our tests should be super simple, the assertion should be expect(sayHello("Pat")).toBe("Hello, Pat!")
 describe('sayHello', function() {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
@@ -24,5 +28,7 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
-
-})
+    it('should return the string "Hello, Jane!" when executed', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+});
